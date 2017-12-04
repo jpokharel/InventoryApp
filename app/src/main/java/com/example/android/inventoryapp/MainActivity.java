b896dacdfcdfe6984e9acb7a9e985fb8c005de94
jiwanpokharel89 <jpokharel@luc.edu>
2017-12-03 18:33:35 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/inventoryapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.android.inventoryapp;
+
+import android.content.ContentUris;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.android.inventoryapp.data.InventoryContract;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EditInventory.class);
+                startActivity(intent);
+            }
+        });
+
+        ListView listView = (ListView) findViewById(R.id.list_item);
+        View emptyView = findViewById(R.id.empty_view);
+        listView.setEmptyView(emptyView);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, EditInventory.class);
+                Uri uri = ContentUris.withAppendedId(InventoryContract.InventoryEntry.CONTENT_URI,id);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_page_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_add:
+                addInventory();
+                break;
+            case R.id.action_delete_all:
+                deleteAll();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
